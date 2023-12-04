ab39b4eae1b4dac1efb97572d53852bc11afb149
곽민지 <mini0006@naver.com>
2023-12-04 16:04:09 +0900

    [GPT] 웹개발 종합반 5주차

--- Project/app.py
+++ Project/app.py
@@ -3,31 +3,76 @@
 0. Flask : 웹서버를 시작할 수 있는 기능. app이라는 이름으로 플라스크를 시작한다
 1. render_template : html파일을 가져와서 보여준다
 '''
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    context = {
-        'name': '민지',
-        'motto': '어제보다 나은 오늘을 보내기'
-    }
-    return render_template('motto.html', data = context)
+import os
+from flask_sqlalchemy import SQLAlchemy
 
-@app.route('/music')
+basedir = os.path.abspath(os.path.dirname(__file__))
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+        'sqlite:///' + os.path.join(basedir, 'database.db')
+
+db = SQLAlchemy(app)
+
+class Song(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), nullable=False)
+    artist = db.Column(db.String(100), nullable=False)
+    title = db.Column(db.String(100), nullable=False)
+    image_url = db.Column(db.String(10000), nullable=False)
+
+    def __repr__(self):
+        return f'{self.title} {self.artist} 추천 by {self.username}'
+
+with app.app_context():
+    db.create_all()
+
+@app.route("/")
+def home():
+    name = '최지웅'
+    motto = "행복해서 웃는게 아니라 웃어서 행복합니다."
+
+    context = {
+        "name": name,
+        "motto": motto,
+    }
+    return render_template('motto.html', data=context)
+
+@app.route("/music/")
 def music():
-    return render_template('music.html')
+    song_list = Song.query.all()
+    return render_template('music.html', data = song_list)
 
 @app.route("/iloveyou/<name>/")
 def iloveyou(name):
+    motto = f"{name}야 난 너뿐이야!"
 
-    motto = f"{name}야 난 너뿐이야..."
     context = {
-        "name": name,
-        "motto": motto
+        'name': name,
+        'motto': motto,
     }
+    return render_template('motto.html', data=context)
 
-    return render_template("motto.html", data=context)
+@app.route("/music/create/")
+def music_create():
+    username_receive = request.args.get('username')
+    title_receive = request.args.get('title')
+    artist_receive = request.args.get('artist')
+    image_url_receive = request.args.get('image_url')
+
+    song = Song(username = username_receive, title = title_receive, artist = artist_receive, image_url = image_url_receive)
+    db.session.add(song)
+    db.session.commit()
+
+    return redirect(url_for('render_music_filter', username = username_receive))
+
+@app.route("/music/<username>/")
+def render_music_filter(username):
+    filter_list = Song.query.filter_by(username = username).all()
+    return render_template('music.html', data = filter_list)
+    
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8080)
+    app.run(debug=True)
